b3249bd59b88dc16679be61821cc98bc0dd624bd
logdanek <113017608+logdanek@users.noreply.github.com>
2022-09-27 23:28:41 +0300

    Комментирование всех строк подряд

--- task1.py
+++ task1.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-import numpy
-import matplotlib.pyplot as mpp
+import math #импорт бибилиотеки 
+import numpy #импорт бибилиотеки
+import matplotlib.pyplot as mpp #импорт бибилиотеки
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-if __name__=='__main__':
-    arguments = numpy.arange(0, 200, 0.1)
-    mpp.plot(
+if __name__=='__main__': #что-то похожее на C++
+    arguments = numpy.arange(0, 200, 0.1) #по всей видимости создание списка
+    mpp.plot( #вызов функции
         arguments,
         [math.cos(a) * math.sin(a/50.0) for a in arguments]
     )
-    mpp.show()
+    mpp.show() #вывод полученных вкусняшек на экран
